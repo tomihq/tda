950481790bd26f31f7e941b5de3b58039f8d95f4
Tom <hernandeztomas584@gmail.com>
2025-04-24 20:17:46 -0300

    add: fuertemente conexo

--- codes/graphs/connected_graph.py
+++ codes/graphs/connected_graph.py
@@ -1,5 +1,6 @@
 from dfs import dfs
 '''
+    Precondición: Grafo no dirigido.
     Objetivo: Recorrer cada componente conexa y si todas lo son, el grafo G es conexo.
     1. Recibo grafo en forma de lista de adyacencias. Empiezo desde indice 0 (nodo 1), cada aparicion del nodo luego al final hay que sumarle 1 pues el 0 es el nodo 1.
     2. Empiezo desde un nodo particular, lo guardo como visitado (como voy a recorrer mas de una vez un nodo por el for mas externo, antes de hacer dfs me fijo si no esta en la lista, si no esta hago dfs.) y me fijo sus vecinos. Si el vecino w no esta en la lista hago dfs. Esto indica que el inicio de "dfs" lo tengo que hacer varias veces porque con una única vez recorro los vecinos del primer nodo. 
---
+++ codes/graphs/is_acyclic_graph.py
@@ -0,0 +1,5 @@
+'''
+    Precondición: Es un grafo dirigido acíclico. De lo contrario, no tiene sentido hacer un orden topológico porque es una dependencia circular.
+    Ej.: A -> B -> C -> A como para hacer A necesito A, B y C no tiene sentido
+'''
+
