7db63c47354c98d73664d47437576a212514d992
= <hernandeztomas584@gmail.com>
2025-05-18 20:05:18 -0300

    add: suma_selectiva

--- codes/greedy/parejas_baile.py
+++ codes/greedy/parejas_baile.py
@@ -1,12 +1,12 @@
 def parejas_baile(c1, c2): 
-    c1 = sorted(c1)
-    c2 = sorted(c2)
+    c1 = sorted(c1) # O(n log n)
+    c2 = sorted(c2) # O(k log k)
     i = 0; 
     j = 0; 
     counter = 0; 
 
-    while(i < len(c1) and j < len(c2)):
-        sub = abs(c1[i] - c2[j]);
+    while(i < len(c1) and j < len(c2)): #O(n + k)
+        sub = abs(c1[i] - c2[j]); #O(1)
         if(sub <= 1):
             counter+=1;
             i+=1;
@@ -17,7 +17,7 @@ def parejas_baile(c1, c2):
             i+=1;
     
     return counter 
-
+# Complejidad total O(n log n + k log k + n + k) = O(n log n + k log k)
 print(parejas_baile([5], [1, 4])) #1
 print(parejas_baile([1, 4], [5])) #1
 print(parejas_baile([1, 2, 3], [2, 3, 4])) #3
